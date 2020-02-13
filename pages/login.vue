311046bd514930c5ed1e9e8729e7eb4bac4e0838
sergey <SergeyGuliy>
2020-02-14 00:16:49 +0200

    -Created login.vue page

--- pages/login.vue
+++ pages/login.vue
@@ -1,22 +1,80 @@
 <template>
-  <div>
-    <form @submit.prevent="submitLogIn">
-      <div><input v-model="email" type="email" placeholder="email" /></div>
-      <div>
-        <input v-model="password" type="password" placeholder="password" />
-      </div>
-      <div><input type="submit" />Отправить</div>
-    </form>
+  <div id="Login">
+    <v-card width="370" raised>
+      <v-card-title class="headline">Log In</v-card-title>
+      <v-divider />
+      <v-card-text>
+        <v-form ref="form" v-model="valid">
+          <v-text-field
+            v-model="email"
+            :rules="rules.emailRules"
+            :counter="25"
+            label="Input E-mail"
+            required
+            outlined
+          />
+          <v-text-field
+            v-model="password"
+            :rules="rules.password"
+            :counter="16"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+            :type="showPassword ? 'text' : 'password'"
+            label="Input password"
+            required
+            outlined
+          />
+          <v-row>
+            <v-col cols="6">
+              <v-btn
+                :disabled="!valid"
+                @click="submitLogIn"
+                color="success"
+                class="mr-4"
+                block
+              >
+                Validate
+              </v-btn>
+            </v-col>
+            <v-col cols="6">
+              <v-btn
+                @click="$router.push('/registration')"
+                color="success"
+                block
+              >
+                Registration
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login',
+  name: 'Login',
   data() {
     return {
+      showPassword: false,
+      valid: false,
       email: '',
-      password: ''
+      password: '',
+      rules: {
+        emailRules: [
+          (v) => !!v || 'E-mail is required',
+          (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+        ],
+        password: [
+          (v) => !!v || 'Password is required',
+          (v) => !(v.length < 8) || 'Password must longer 7',
+          (v) =>
+            /^(?=.*\d)(?=.*[a-zA-Z]).{1,20}$/.test(v) ||
+            'Password must contains letters and numbers',
+          (v) => !(v.length > 16) || "Password can't be longer 16"
+        ]
+      }
     }
   },
   layout: 'empty',
@@ -41,4 +99,7 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style lang="sass">
+#Login
+  margin: 0 auto
+</style>
